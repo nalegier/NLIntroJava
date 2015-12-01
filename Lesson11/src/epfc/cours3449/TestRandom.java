eae5b7797aeb4b8decd779de7ff38b67ca188ef6
0101nalegier <0101nalegier@PC8821.domepfc.net>
2015-12-01 20:48:55 +0100

    Change code, add system out

--- Lesson11/src/epfc/cours3449/TestRandom.java
+++ Lesson11/src/epfc/cours3449/TestRandom.java
@@ -6,24 +6,32 @@
 package epfc.cours3449;
 
 import java.util.Random;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class TestRandom {
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Entrez 2 chiffres: ");
-        int value = keyboard.nextInt();
-        int value2 = keyboard.nextInt();
-        PrintRandom(value);
-        PrintRandom(value2);
-        
-    }
-        public static void PrintRandom(int value){
-            Random r1 = new Random(value);
+        //Scanner keyboard = new Scanner(System.in);
+        //System.out.print("Entrez 2 chiffres: ");
+        //int value = keyboard.nextInt();
+        //int value2 = keyboard.nextInt();
+        //PrintRandom(value);
+        //PrintRandom(value2);
+        for (int j = 0; j < 2; j++){
+            Random r1 = new Random(3);
             for(int i = 0; i < 10; i++){
                 System.out.print(r1.nextInt(1000) + " ");
             }
             System.out.println();
         }
+        
+    }
+}
+        /*public static void PrintRandom(int value){
+            Random r1 = new Random(value);
+            for(int i = 0; i < 10; i++){
+                System.out.print(r1.nextInt(1000) + " ");
+            }
+            System.out.println();
+        /*}
 }
 
