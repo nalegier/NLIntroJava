b5b0b43e55a02032e7d1d8aaef638710b7e457ba
Natacha <Natacha@192.168.1.3>
2016-02-28 14:03:04 +0100

    commit devoir

---
+++ Github/src/epfc/cours3449L21Devoir/DemoPersistEncaps.java
@@ -0,0 +1,50 @@
+package epfc.cours3449L21Devoir;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DemoPersistEncaps {
+
+    static Catalogue cat;
+    static final String nomDeFichier = "livre.csv";
+
+    public static void main(String[] args) throws FileNotFoundException, IOException, Exception {
+        cat = new Catalogue(nomDeFichier);
+        lireLeCatalogue();
+        ///creeDeuxLivres();
+        ///testDeleteNoLivre();        
+        ///lireLeCatalogue();
+        ///TestReadByAuteur();
+    }
+/*
+    private static void TestReadByAuteur() {
+        System.out.println("\n\n---------------------------------------------------------------");
+        ArrayList<Livre> livresDeVictor = cat.readByAuteur("Victor");
+        for (Livre livre : livresDeVictor) {
+            System.out.println(livre.toString());
+        }
+    }
+*/
+    private static void lireLeCatalogue() throws FileNotFoundException {
+        cat.readFile();
+        }
+    }
+/*
+    private static void testDeleteNoLivre() {
+        try {
+            Livre l = new Livre("test", "auteur");
+            cat.delete(l.getId());
+        } catch (Exception ex) {
+            System.out.println( ex);
+        }
+    }
+
+    private static void creeDeuxLivres() throws FileNotFoundException {
+        Livre l1 = new Livre("Notre Dame", "Victor");
+        cat.add(l1);
+        Livre l2 = new Livre("Java ...", "Orstmann");
+        cat.add(l2);
+    }
+}*/
+    
