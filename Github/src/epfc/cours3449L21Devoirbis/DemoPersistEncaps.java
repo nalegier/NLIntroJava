7b6bd415aa65bdf5d511f44058b6edc726aab589
Natacha <Natacha@192.168.1.3>
2016-03-02 20:56:31 +0100

    commit devoir

--- Github/src/epfc/cours3449L21Devoirbis/DemoPersistEncaps.java
+++ Github/src/epfc/cours3449L21Devoirbis/DemoPersistEncaps.java
@@ -3,11 +3,13 @@ package epfc.cours3449L21Devoirbis;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DemoPersistEncaps {
 
     static Catalogue cat;
     static final String nomDeFichier = "livre.txt";
+     static Scanner keyb = new Scanner(System.in);
 
     public static void main(String[] args) throws FileNotFoundException, IOException, Exception {
         cat = new Catalogue(nomDeFichier);
@@ -34,7 +36,7 @@ public class DemoPersistEncaps {
         }
     }
 
-    private static void testDeleteNoLivre() {
+    /*private static void testDeleteNoLivre() {
         try {
             Livre l = new Livre("test", "auteur");
             cat.delete(l.getId());
@@ -42,11 +44,30 @@ public class DemoPersistEncaps {
             System.out.println( ex);
         }
     }
-
+    */
+    
     private static void creeDeuxLivres() throws FileNotFoundException, IOException {
         Livre l1 = new Livre("Notre Dame", "Victor");
         cat.add(l1);
         Livre l2 = new Livre("Java ...", "Orstmann");
         cat.add(l2);
+    }
+    
+    
+        private static void delete1Livre() throws FileNotFoundException, IOException, Exception { 
+        System.out.println("Please give the identifiant of the book to delete");
+        int identifiant = keyb.nextInt(); 
+        cat.delete(identifiant);
+    }
+
+    private static void updateLivre() throws IOException {
+        System.out.println("Please give the identifiant of the book to update: ");
+        int identifiant = keyb.nextInt();
+        System.out.println("New Title: ");
+        String newtitre = keyb.nextLine();
+        System.out.println("New Autor: ");
+        String newauteur = keyb.nextLine();
+        cat.add(new Livre(identifiant, newtitre, newauteur));
+                
     }
 }
