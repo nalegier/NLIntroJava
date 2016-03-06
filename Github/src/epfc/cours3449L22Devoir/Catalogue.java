f2063d803acfc173d0cbbb408841f4e0f05281ee
Natacha <Natacha@192.168.1.4>
2016-03-06 12:23:56 +0100

    Corrective Catalogue Sans cacher - Version Natacha

---
+++ Github/src/epfc/cours3449L22Devoir/Catalogue.java
@@ -0,0 +1,95 @@
+package epfc.cours3449L22Devoir;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Catalogue {
+   
+    public Catalogue(){
+ }
+    
+    public void add(Livre l) {
+        // insert with value for title and author
+        try{
+            Class.forName("com.mysql.jdbc.Driver");  //charger le driver pour MySql
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/librarie", "root", "");
+            String addsql = "insert into catalogue (titre,auteur)" + "values (?,?)";
+            PreparedStatement preparedStat = connection.prepareStatement(addsql);
+            String autor = l.getAuteur();
+            String title = l.getTitre();
+            preparedStat.setString(1, title);
+            preparedStat.setString(2, autor);
+            preparedStat.executeQuery();
+            connection.close();
+    }
+    catch (Exception e){
+        {
+            System.err.println("Got an exception!");
+            System.err.println(e.getMessage());
+        }
+    } 
+    }
+
+    public void update(Livre l) throws Exception {
+        //update based on ID
+
+    }
+
+
+    public void delete(int id) throws Exception {
+  
+    }
+    }
+
+    ArrayList<Livre> read() {
+        try{
+        
+            Class.forName("com.mysql.jdbc.Driver");  //charger le driver pour MySql
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/librarie", "root", "nanou1831");
+            Statement statement = connection.createStatement();
+             
+            // to execute a select
+            ResultSet resultSet = statement.executeQuery
+            ("select * from catalogue");
+             
+            ArrayList<Livre> listelivres = new ArrayList<>();
+            // to show the result of the select command
+            while (resultSet.next()) {
+                System.out.println
+                (resultSet.getString(1) + " - " +
+                resultSet.getString(2) + " - " + 
+                resultSet.getString(3));
+                Livre l = new Livre(
+                     resultSet.getInt(1), 
+                     resultSet.getString(2), 
+                     resultSet.getString(3));
+                listelivres.add(l);
+        }
+        for (Livre livre: listelivres){
+            System.out.println(livre.toString());
+        }
+        connection.close();
+    }
+    catch (Exception e) {
+        System.err.println("Got an exception !!!");
+        System.err.println(e.getMessage());
+}    
+}
+    
+    /*
+    ArrayList<Livre> readByAuteur(String auteur) {
+        ArrayList<Livre> lsDeAuteur = new ArrayList<>();
+        for (Livre livre : ls) {
+            if (livre.getAuteur().equals(auteur)) {
+                lsDeAuteur.add(livre);
+            }
+        }
+        return lsDeAuteur;
+    }
+*/
+
+    }
---
+++ Github/src/epfc/cours3449L22Devoir/DemoPersistEncaps.java
@@ -0,0 +1,104 @@
+package epfc.cours3449L22Devoir;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DemoPersistEncaps {
+
+    static Catalogue cat;
+    //static final String nomDeFichier = "livre.txt";
+    static Scanner keyb = new Scanner(System.in);
+
+    public static void main(String[] args) throws FileNotFoundException, IOException, Exception {
+        cat = new Catalogue();
+        menu();
+        
+    }
+
+    private static void menu() throws IOException, Exception {
+        boolean next = true;
+        while(next){
+        System.out.println("\n       Menu ");
+        System.out.println("--------------------------------------");
+        System.out.println("If you want to add a book, enter A");
+        System.out.println("If you want to delete a book, enter D");
+        System.out.println("If you want to find a book, enter F");
+        System.out.println("If you want to update a book, enter U");
+        System.out.println("If you want to list the catalogue, enter R");
+        System.out.println("To stop, press E");
+        System.out.println("\n --------------------------------------");
+        String choix = keyb.nextLine();
+            choix = choix.toUpperCase();
+            if (choix.length() == 0){
+                choix = keyb.nextLine();
+            }
+            switch(choix){
+                case "A":
+                    add1Livre();
+                    break;
+                case "D":
+                    delete1Livre();
+                    break;
+                case "F":
+                    find1Livre();
+                    break;     
+                case "U":
+                    updateLivre();
+                    break;
+                case "R":
+                    lireLeCatalogue();
+                    break;
+                case "E":
+                    next = false;
+                    break;
+                default:
+                    System.out.println("Incorrect choice");
+                   }
+        }
+    }
+    
+    private static void lireLeCatalogue() throws IOException {
+        ArrayList<Livre> sendCat = cat.read();
+        for (Livre livre : sendCat) {
+            System.out.println(livre.toString());
+        }
+    }
+    
+    private static void add1Livre() throws FileNotFoundException, IOException, ClassNotFoundException {
+        System.out.println("Enter a title: ");
+        String title = keyb.nextLine();
+        System.out.println("Enter an autor: ");
+        String autor = keyb.nextLine();
+        Livre l1 = new Livre(title, autor);
+        cat.add(l1);
+    }
+    
+    private static void delete1Livre() throws FileNotFoundException, IOException, Exception { 
+        System.out.println("Please give the identifiant of the book to delete");
+        int identifiant = keyb.nextInt(); 
+        cat.delete(identifiant);
+    }
+
+    private static void updateLivre() throws IOException, Exception {
+        System.out.println("Please give the identifiant of the book to update: ");
+        int identifiant = Integer.parseInt(keyb.nextLine());
+        System.out.println("New Title: ");
+        String newtitre = keyb.nextLine();
+        System.out.println("New Autor: ");                
+        String newauteur = keyb.nextLine();
+        cat.update(identifiant, new Livre(identifiant, newtitre, newauteur));
+        
+                
+    }
+    
+    private static void find1Livre() throws IOException {
+        System.out.println("Please enter the Autor you want to search");
+        String autor = keyb.nextLine();
+        ArrayList<Livre> livresDeVictor = cat.readByAuteur(autor);
+        for (Livre livre : livresDeVictor) {
+            System.out.println(livre.toString());
+        }
+    }
+}
